2d669dbf80f113656bd5d0d36e65ece95e2237b1
estanho <estanhoeu@gmail.com>
2025-01-17 10:57:56 -0300

    fix: wrong params for background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,73 +52,78 @@ export default async function Home() {
   const date = new Date(climate?.data.current.time);
 
   return (
-    <div className="flex min-h-screen items-center justify-center p-4">
-      <Card className="w-full max-w-4xl">
-        <CardHeader>
-          <CardTitle>
-            <div className="flex items-center justify-center space-x-2">
-              <h1 className="text-2xl font-extrabold tracking-tight sm:text-3xl">
-                Tempo em Gravataí, RS
-              </h1>
-              <MapPinIcon className="h-6 w-6 sm:h-7 sm:w-7" strokeWidth={2.5} />
-            </div>
-          </CardTitle>
-          <CardDescription>
-            <div className="flex items-center justify-center gap-2">
-              <p className="text-muted-foreground">
-                Última atualização:{" "}
-                {formatterDate(date, { dateStyle: "short" })}
-                {", "}
-                {formatterDate(date, { timeStyle: "short" })}
-              </p>
-            </div>
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-            <WeatherMetric
-              value={climate?.data.current.temperature_2m}
-              unit="ºC"
-              label="Temperatura atual"
-              icon={ThermometerIcon}
-            />
-            <WeatherMetric
-              value={climate?.data.current.apparent_temperature}
-              unit="ºC"
-              label="Sensação térmica"
-              icon={ThermometerIcon}
-            />
-            <WeatherMetric
-              value={climate?.data.daily.precipitation_probability_max[1]}
-              unit="%"
-              label="Chance de chuva"
-              icon={CloudRainWindIcon}
-            />
-            <WeatherMetric
-              value={climate?.data.current.wind_speed_10m}
-              unit=" km/h"
-              label="Velocidade do vento"
-              icon={WindIcon}
-            />
-            <WeatherMetric
-              value={climate?.data.current.relative_humidity_2m}
-              unit="%"
-              label="Umidade relativa"
-              icon={DropletIcon}
-            />
-            <WeatherMetric
-              value={climate?.data.current.wind_direction_10m}
-              unit="°"
-              label="Direção do vento"
-              icon={WindIcon}
-            />
-          </div>
-        </CardContent>
-      </Card>
+    <>
       <BackgroundTransition
-        sunrise={climate?.data.daily.sunrise}
-        sunset={climate?.data.daily.sunset}
+        sunrise={climate?.data.daily.sunrise[1]}
+        sunset={climate?.data.daily.sunset[1]}
       />
-    </div>
+      <div className="flex min-h-screen items-center justify-center p-4">
+        <Card className="w-full max-w-4xl">
+          <CardHeader>
+            <CardTitle>
+              <div className="flex items-center justify-center space-x-2">
+                <h1 className="text-2xl font-extrabold tracking-tight sm:text-3xl">
+                  Tempo em Gravataí, RS
+                </h1>
+                <MapPinIcon
+                  className="h-6 w-6 sm:h-7 sm:w-7"
+                  strokeWidth={2.5}
+                />
+              </div>
+            </CardTitle>
+            <CardDescription>
+              <div className="flex items-center justify-center gap-2">
+                <p className="text-muted-foreground">
+                  Última atualização:{" "}
+                  {formatterDate(date, { dateStyle: "short" })}
+                  {", "}
+                  {formatterDate(date, { timeStyle: "short" })}
+                </p>
+              </div>
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+              <WeatherMetric
+                value={climate?.data.current.temperature_2m}
+                unit="ºC"
+                label="Temperatura atual"
+                icon={ThermometerIcon}
+              />
+              <WeatherMetric
+                value={climate?.data.current.apparent_temperature}
+                unit="ºC"
+                label="Sensação térmica"
+                icon={ThermometerIcon}
+              />
+              <WeatherMetric
+                value={climate?.data.daily.precipitation_probability_max[1]}
+                unit="%"
+                label="Chance de chuva"
+                icon={CloudRainWindIcon}
+              />
+              <WeatherMetric
+                value={climate?.data.current.wind_speed_10m}
+                unit=" km/h"
+                label="Velocidade do vento"
+                icon={WindIcon}
+              />
+              <WeatherMetric
+                value={climate?.data.current.relative_humidity_2m}
+                unit="%"
+                label="Umidade relativa"
+                icon={DropletIcon}
+              />
+              <WeatherMetric
+                value={climate?.data.current.wind_direction_10m}
+                unit="°"
+                label="Direção do vento"
+                icon={WindIcon}
+              />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 }
